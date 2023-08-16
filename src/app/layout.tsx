78fcba4e6b04eea663f159bcffaf064ec8bb2e63
Chris Berger <flippinberger@gmail.com>
2023-08-16 15:36:14 -0600

    Add new socials; Hide unfinished pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
 import NavItem from './nav-item';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,13 +20,42 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="flex flex-col w-32 pl-4 mt-4 gap-2 fixed top-1 left-1">
           <NavItem path='/' title='Chris' />
-          <NavItem path='/pages/projects' title='Projects' />
+          <NavItem path='/pages/about' title='About' />
+          {/* <NavItem path='/pages/projects' title='Projects' />
           <NavItem path='/pages/birds' title='Birds' />
           <NavItem path='/pages/adventures' title='Adventures' />
-          <NavItem path='/pages/blog' title="Blog" />
+          <NavItem path='/pages/blog' title="Blog" /> */}
         </div>
         {children}
+
+        <div className="flex flex-col p-3 m-3 gap-2 bg-emerald-50 fixed bottom-1 right-1 rounded-full">
+          <SocialLink url='https://github.com/flippinberger' imagePath='/git.svg' />
+          <SocialLink url='https://open.spotify.com/user/chrisbergerr?si=bd328fafb9644782' imagePath='/spotify.svg' />
+          <SocialLink url='https://www.goodreads.com/user/show/161613962-chris-berger' imagePath='/goodreads.svg' />
+          <SocialLink url='https://www.linkedin.com/in/christopher-berger-76b2848a' imagePath='/linkedin-color.svg' />
+          <SocialLink url='mailto:flippinberger@gmail.com' imagePath='/mail.svg' />
+          {/* <SocialLink url='https://twitter.com/drchrisberger' imagePath='/twitter.svg' /> */}
+        </div>
       </body>
     </html>
+  )
+}
+
+interface SocialLinkProps {
+  url: string,
+  imagePath: string,
+}
+
+const SocialLink: React.FC<SocialLinkProps> = ({ url, imagePath }) => {
+  return (
+    <a href={url} target='_blank'>
+      <Image
+        src={imagePath}
+        width={32}
+        height={32}
+        alt={`${url}`}
+        className='hover:animate-spin'
+      />
+    </a>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import TechStack from './tech-stack'
 
 export default function Home() {
@@ -7,37 +6,10 @@ export default function Home() {
       <div className="flex flex-col w-full min-h-screen items-center justify-center px-64">
         <h1 className="mb-2 mt-0 text-6xl font-medium leading-tight text-emerald-50">Hey, I'm Chris</h1>
         <h3 className="text-4xl font-medium text-emerald-50 pb-16">I'm a full stack web developer with over 6 years of experience in industries ranging from office furniture to security.<br /><br />Below are some of my most used technologies.</h3>
-
         <TechStack />
-
-        <div className="flex flex-col p-3 m-3 gap-2 bg-emerald-50 fixed bottom-1 right-1 rounded-full">
-          <SocialLink url='https://github.com/flippinberger' imagePath='/git.svg' />
-          <SocialLink url='https://twitter.com/drchrisberger' imagePath='/twitter.svg' />
-          <SocialLink url='https://www.linkedin.com/in/christopher-berger-76b2848a' imagePath='/linkedin-color.svg' />
-          <SocialLink url='mailto:flippinberger@gmail.com' imagePath='/mail.svg' />
-        </div>
       </div>
     </main>
   )
 }
 
-interface SocialLinkProps {
-  url: string,
-  imagePath: string,
-}
-
-const SocialLink: React.FC<SocialLinkProps> = ({ url, imagePath }) => {
-  return (
-    <a href={url} target='_blank'>
-      <Image
-        src={imagePath}
-        width={32}
-        height={32}
-        alt={`${url}`}
-        // className='hover:border-emerald-200 hover:border-2 hover:rounded-full'
-        className='hover:animate-spin'
-      />
-    </a>
-  )
-}
 
